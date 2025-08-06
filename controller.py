10b415f5c9f23d85bdaaafb5efb95ea54788dd95
Wensen Liu <wensenl@andrew.cmu.edu>
2025-08-06 19:39:13 -0400

    Testing with barrier functions and trajectory free MPC

--- controller.py
+++ controller.py
@@ -4,7 +4,7 @@ import cyipopt as ipopt
 import os
 
 class TrajOpt:
-    def __init__(self, model: VesselModel, N, ns, nu, dt, Qvec, Rvec, Qfvec, s0, sf):
+    def __init__(self, model: VesselModel, N, ns, nu, dt, Qvec, Rvec, Qfvec, s0, sf, obstacles):
         self.model = model
         self.N = N
         self.ns = ns
@@ -24,6 +24,9 @@ class TrajOpt:
         # Control clamps
         self.u_min = -10
         self.u_max = 10
+
+        # Obstacles
+        self.obs = obstacles
 
         self.Z0 = self.make_initial_guess()
         assert len(self.Z0) == N*ns + (N-1)*nu, "Length of initial guess does not match length calculated from given N, nx, and nu"
@@ -46,21 +49,6 @@ class TrajOpt:
         inputs = inputs_flat.reshape((self.N-1, self.nu))
         return states, inputs
     
-    def LQRcost(self, Z):
-        states, inputs = self.flat2vec(Z)
-        
-        cost = 0
-        for i in range(self.N-1):
-            sn = states[i]
-            un = inputs[i]
-            # LQR Cost
-            cost += 0.5 * (sn-self.sf) @ self.Q @ (sn-self.sf)
-            cost += 0.5 * un @ self.R @ un
-        
-        cost += (states[-1]-self.sf) @ self.Qf @ (states[-1]-self.sf) # LQR terminal cost
-        
-        return cost
-    
     def LQRcost_fast(self, Z):
         states, inputs = self.flat2vec(Z)
         S = states[:-1]
@@ -73,30 +61,6 @@ class TrajOpt:
         #print(f"cost with inputs: {cost}")
 
         return cost
-    
-    def equality_constraints(self, Z):
-        states, inputs = self.flat2vec(Z)
-
-        constraints = []
-
-        # Initial and terminal constraints
-        initial_constraint = states[0] - self.s0
-        terminal_constraint = states[-1] - self.sf
-        constraints.extend(initial_constraint)
-        constraints.extend(terminal_constraint)
-
-        # Dynamics constraints
-        for i in range(self.N-1):
-            sn = states[i]
-            snp1 = states[i+1]
-            un = inputs[i]
-            #dyn_snp1 = self.model.euler(sn, un, self.dt)
-            #dynamics_constraint = snp1 - dyn_snp1
-            dynamics_constraint = self.model.hermite_simpson(sn, snp1, un, self.dt)
-            constraints.extend(dynamics_constraint)
-
-        assert np.all(np.isfinite(constraints)), "NaN or Inf in constraints"
-        return np.array(constraints)
     
     def equality_constraints_fast(self, Z):
         states, inputs = self.flat2vec(Z)
@@ -122,42 +86,19 @@ class TrajOpt:
         assert np.all(np.isfinite(constraints)), "NaN or Inf in constraints"
         return constraints.flatten()
     
-    def inequality_constraints(self, Z):
-        states, inputs = self.flat2vec(Z)
-
-        constraints = []
-
-        # Control clamping
-        for i in range(self.N-1):
-            un = inputs[i]
-            constraints.extend(un-self.u_min)
-            constraints.extend(self.u_max-un)
-
-        # Simulated obstacles
-        for i in range(self.N):
-            sn = states[i]
-            X = sn[0]
-            Y = sn[1]
-            oc1 = (X-0.25)**2 + (Y-0.25)**2 - 0.1**2
-            oc2 = (X-0.6)**2 + (Y-0.4)**2 - 0.1**2
-            oc3 = (X-0.75)**2 + (Y-0.75)**2 - 0.1**2
-            constraints.extend([oc1, oc2, oc3])
-
-        assert np.all(np.isfinite(constraints)), "NaN or Inf in constraints"
-        return np.array(constraints)
-    
     def inequality_constraints_fast(self, Z):
         states, inputs = self.flat2vec(Z)
 
         constraints_min_controls = inputs - self.u_min
         constraints_max_controls = -inputs + self.u_max
+        constraints = np.concatenate((constraints_min_controls.flatten(), constraints_max_controls.flatten()))
+                                      
         X = states[:,0]
         Y = states[:,1]
-        oc1 = (X-0.25)**2 + (Y-0.25)**2 - 0.1**2
-        oc2 = (X-0.8)**2 + (Y-0.3)**2 - 0.25**2
-        oc3 = (X-0.75)**2 + (Y-0.75)**2 - 0.1**2
-        oc4 = (X-0.3)**2 + (Y-0.8)**2 - 0.25**2
-        constraints = np.concatenate((constraints_min_controls.flatten(), constraints_max_controls.flatten(), oc1.flatten(), oc2.flatten(), oc3.flatten(), oc4.flatten()))
+        for obstacle in self.obs:
+            obs_x, obs_y, obs_r = obstacle
+            oc = (X-obs_x)**2 + (Y-obs_y)**2 - obs_r**2
+            constraints = np.concatenate((constraints, oc.flatten()))
 
         assert np.all(np.isfinite(constraints)), "NaN or Inf in constraints"
         return np.array(constraints)
@@ -172,14 +113,14 @@ class TrajOpt:
             fun=self.LQRcost_fast,
             x0=self.Z0,
             constraints=constraints,
-            tol=1e-3,
+            tol=5e-5,
             options={"disp": 5,
                      'linear_solver': 'ma57',
                      'hsllib': f"{os.environ['CONDA_PREFIX']}/lib/x86_64-linux-gnu/libcoinhsl.so"}
         )
 
 class MPC:
-    def __init__(self, model: VesselModel, N, ns, nu, dt, Qvec, Rvec, Qfvec, s0, sf, sref):
+    def __init__(self, model: VesselModel, N, ns, nu, dt, Qvec, Rvec, Qfvec, s0, sf, sref, obstacles, radii):
         self.model = model
         self.N = N # this is the MPC window size, not the N of the entire trajectory
         self.ns = ns
@@ -200,6 +141,10 @@ class MPC:
         # Control clamps
         self.u_min = -10
         self.u_max = 10
+
+        # Obstacle states
+        self.obs = obstacles
+        self.radii = radii
 
         self.Z0 = self.make_initial_guess()
         assert len(self.Z0) == N*ns + (N-1)*nu, "Length of initial guess does not match length calculated from given N, nx, and nu"
@@ -235,6 +180,27 @@ class MPC:
 
         return cost
     
+    def LQRcost_dist(self, Z):
+        states, inputs = self.flat2vec(Z)
+        cost = 0
+        cost += np.sum(0.5 * np.vecdot((states-self.sf), np.multiply(self.Qvec, states-self.sf)))
+        cost += np.sum(0.5 * np.vecdot(inputs, np.multiply(self.Rvec, inputs)))
+
+        # "Soft" Barrier function for obstacles
+        """
+        for i in range(len(self.radii)):
+            obstacle = self.obs[i]
+            radius = self.radii[i]
+
+            xys = states-obstacle
+            rs = np.sum(xys[:, 0:2] ** 2, axis=1)
+            b = rs - (radius**2)
+            bcost = np.exp(-100*b)
+            cost += np.sum(bcost)
+        """
+
+        return cost
+    
     def equality_constraints_fast(self, Z):
         states, inputs = self.flat2vec(Z)
 
@@ -266,6 +232,20 @@ class MPC:
         constraints_max_controls = -inputs + self.u_max
         
         constraints = np.concatenate((constraints_min_controls.flatten(), constraints_max_controls.flatten()))
+        #print(f"constraints = {constraints}")
+
+        # Obstacle constraints
+        """
+        for i in range(len(self.radii)):
+            obstacle = self.obs[i]
+            radius = self.radii[i]
+
+            xys = states-obstacle
+            rs = np.sum(xys[:, 0:2] ** 2, axis=1)
+            b = rs - (radius**2)
+            #print(f"b = {b}")
+            constraints = np.concatenate((constraints, b.flatten()))
+        """
 
         assert np.all(np.isfinite(constraints)), "NaN or Inf in constraints"
         return np.array(constraints)
@@ -277,10 +257,11 @@ class MPC:
         ]
 
         self.sol = ipopt.minimize_ipopt(
-            fun=self.LQRcost_fast,
+            #fun=self.LQRcost_fast,
+            fun=self.LQRcost_dist,
             x0=self.Z0,
             constraints=constraints,
-            tol=1e-3,
+            tol=1e-4, # default 1e-3
             options={"disp": 5,
                      'linear_solver': 'ma57',
                      'hsllib': f"{os.environ['CONDA_PREFIX']}/lib/x86_64-linux-gnu/libcoinhsl.so"}
