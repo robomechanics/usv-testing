11c8e36403e8f3b5045df97ca57412949feb6356
Wensen Liu <wensenl@andrew.cmu.edu>
2025-08-07 17:13:48 -0400

    salom course from dircol

--- controller.py
+++ controller.py
@@ -113,14 +113,14 @@ class TrajOpt:
             fun=self.LQRcost_fast,
             x0=self.Z0,
             constraints=constraints,
-            tol=5e-5,
+            tol=1e-3,
             options={"disp": 5,
                      'linear_solver': 'ma57',
                      'hsllib': f"{os.environ['CONDA_PREFIX']}/lib/x86_64-linux-gnu/libcoinhsl.so"}
         )
 
 class MPC:
-    def __init__(self, model: VesselModel, N, ns, nu, dt, Qvec, Rvec, Qfvec, s0, sf, sref, obstacles, radii):
+    def __init__(self, model: VesselModel, N, ns, nu, dt, Qvec, Rvec, Qfvec, s0, sf, sref, obstacles):
         self.model = model
         self.N = N # this is the MPC window size, not the N of the entire trajectory
         self.ns = ns
@@ -139,12 +139,11 @@ class MPC:
         self.sref = sref # reference trajectory MPC is following
 
         # Control clamps
-        self.u_min = -10
+        self.u_min = -10 # default +-10
         self.u_max = 10
 
-        # Obstacle states
+        # Obstacles
         self.obs = obstacles
-        self.radii = radii
 
         self.Z0 = self.make_initial_guess()
         assert len(self.Z0) == N*ns + (N-1)*nu, "Length of initial guess does not match length calculated from given N, nx, and nu"
@@ -182,20 +181,35 @@ class MPC:
     
     def LQRcost_dist(self, Z):
         states, inputs = self.flat2vec(Z)
+        S = states[:-1]
+
         cost = 0
-        cost += np.sum(0.5 * np.vecdot((states-self.sf), np.multiply(self.Qvec, states-self.sf)))
+        cost += np.sum(0.5 * np.vecdot((S-self.sf), np.multiply(self.Qvec, S-self.sf)))
+        cost += np.sum(0.5 * np.vecdot((states[-1]-self.sf), np.multiply(self.Qfvec, states[-1]-self.sf)))
+        #print(f"cost from states: {cost}")
+
         cost += np.sum(0.5 * np.vecdot(inputs, np.multiply(self.Rvec, inputs)))
+        #print(f"cost with inputs: {cost}")
 
         # "Soft" Barrier function for obstacles
         """
-        for i in range(len(self.radii)):
-            obstacle = self.obs[i]
-            radius = self.radii[i]
+        for obstacle in self.obs:
+            #print(f"obstacle = {obstacle}")
+            obs_xy = obstacle[0:2]
+            obs_r = obstacle[2]
 
-            xys = states-obstacle
-            rs = np.sum(xys[:, 0:2] ** 2, axis=1)
-            b = rs - (radius**2)
-            bcost = np.exp(-100*b)
+            xys = states[:, 0:2]-obs_xy
+            #print(f"xys = {xys}")
+
+            rs = np.sum(xys ** 2, axis=1)
+            #print(f"rs = {rs}")
+
+            b = rs - (obs_r**2)
+            #print(f"b = {b}")
+            
+            bcost = 100 * np.exp(-(100*b+5))
+            #print(f"bcost = {bcost}")
+
             cost += np.sum(bcost)
         """
 
@@ -230,39 +244,48 @@ class MPC:
 
         constraints_min_controls = inputs - self.u_min
         constraints_max_controls = -inputs + self.u_max
-        
         constraints = np.concatenate((constraints_min_controls.flatten(), constraints_max_controls.flatten()))
+
+        X = states[:,0]
+        Y = states[:,1]
+        for obstacle in self.obs:
+            obs_x, obs_y, obs_r = obstacle
+            oc = (X-obs_x)**2 + (Y-obs_y)**2 - obs_r**2
+            constraints = np.concatenate((constraints, oc.flatten()))
+
         #print(f"constraints = {constraints}")
-
-        # Obstacle constraints
-        """
-        for i in range(len(self.radii)):
-            obstacle = self.obs[i]
-            radius = self.radii[i]
-
-            xys = states-obstacle
-            rs = np.sum(xys[:, 0:2] ** 2, axis=1)
-            b = rs - (radius**2)
-            #print(f"b = {b}")
-            constraints = np.concatenate((constraints, b.flatten()))
-        """
 
         assert np.all(np.isfinite(constraints)), "NaN or Inf in constraints"
         return np.array(constraints)
     
-    def exec_MPC(self):
+    def exec_MPC(self, version="with_traj"):
+        print(f"Running MPC Solver Version: {version}")
+
         constraints = [
             {"type": "eq", "fun": self.equality_constraints_fast},
             {"type": "ineq", "fun": self.inequality_constraints_fast}
         ]
 
-        self.sol = ipopt.minimize_ipopt(
-            #fun=self.LQRcost_fast,
-            fun=self.LQRcost_dist,
-            x0=self.Z0,
-            constraints=constraints,
-            tol=1e-4, # default 1e-3
-            options={"disp": 5,
-                     'linear_solver': 'ma57',
-                     'hsllib': f"{os.environ['CONDA_PREFIX']}/lib/x86_64-linux-gnu/libcoinhsl.so"}
-        )
+        if version == "with_traj":
+            print(f"Executed MPC Solver Version: with_traj")
+            self.sol = ipopt.minimize_ipopt(
+                fun=self.LQRcost_fast,
+                x0=self.Z0,
+                constraints=constraints,
+                tol=1e-3, # default 1e-3
+                options={"disp": 5,
+                        'linear_solver': 'ma57',
+                        'hsllib': f"{os.environ['CONDA_PREFIX']}/lib/x86_64-linux-gnu/libcoinhsl.so"}
+            )
+        
+        if version == "without_traj":
+            print(f"Executed MPC Solver Version: without_traj")
+            self.sol = ipopt.minimize_ipopt(
+                fun=self.LQRcost_dist,
+                x0=self.Z0,
+                constraints=constraints,
+                tol=1e-3, # default 1e-3
+                options={"disp": 5,
+                        'linear_solver': 'ma57',
+                        'hsllib': f"{os.environ['CONDA_PREFIX']}/lib/x86_64-linux-gnu/libcoinhsl.so"}
+            )
